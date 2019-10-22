49c023142dc9bf5fbaa8846649c56accb544fb62
winsion <winsion@seektopser.com>
2019-10-22 10:57:14 +0800

    更新

--- pull.py
+++ pull.py
@@ -4,7 +4,7 @@
 # @Email:   1583166253@qq.com
 # @Date:   2019-10-20 01:06:24
 # @Last Modified by:   winsion
-# @Last Modified time: 2019-10-20 12:56:49
+# @Last Modified time: 2019-10-22 10:53:20
 
 import os
 
@@ -13,30 +13,30 @@ statusBranch = input("请输入当前目标分支名:")
 if len(currentBranch) == 0:
 	currentBranch = "winsion"
 if len(statusBranch) == 0:
-	statusBranch = "develop"
+	statusBranch = "dev"
 
 work_dir = os.getcwd()
 
-list1 = ['demo', 'lianshi']
+# 修改自己本地的组件目录
+list1 = ['basebusinesscomponent', 'homepagecomponent', 'loginregistercompoment-', 'routercomponent', 'sportcomponent', 'taskcentercomponent', 'userinfocomponent', 'utilitytoolcomponentoc',
+ 'utilitytoolcomponentswift', 'walletcomponent']
 for x in list1:
 	file_path = os.path.join(work_dir, x) # 拼接完整的路径
 
 	os.chdir(file_path)
-	result = os.system("git status")
+	os.system("git status")
 
 	os.system("git add .")
 	os.system("git stash")
-	
 
 	os.system("git checkout" + " " + statusBranch)
-	os.system("git pull")
+	os.system("git pull origin" + " " + statusBranch)
 
 	os.system("git checkout" + " " + currentBranch)
 	os.system("git rebase" + " " + statusBranch)
 	os.system('git stash pop')
 
-	os.system('git add .')
-	os.system("git commit -m '提交2'")
-	os.system('git push')
-
-
+# 进入主工程  pod install   //修改为自己主工程的目录
+file_path = os.path.join(work_dir, "yyproject的副本") # 拼接完整的路径   
+os.chdir(file_path)
+os.system('pod install')
--- push.py
+++ push.py
@@ -4,28 +4,34 @@
 # @Email:   1583166253@qq.com
 # @Date:   2019-10-20 01:06:24
 # @Last Modified by:   winsion
-# @Last Modified time: 2019-10-20 13:07:35
+# @Last Modified time: 2019-10-22 10:56:56
 
 import os
 
 currentBranch = input("请输入要推送的分支名:")
+if len(currentBranch) == 0:
+	currentBranch = "winsion"
 
 work_dir = os.getcwd()
 
-list1 = ['demo', 'lianshi']
-for item in list1:
-	file_path = os.path.join(work_dir, item) # 拼接完整的路径
+# 修改为自己组件工程的目录
+list1 = ['basebusinesscomponent', 'homepagecomponent', 'loginregistercompoment-', 'routercomponent', 'sportcomponent', 'taskcentercomponent', 'userinfocomponent', 'utilitytoolcomponentoc',
+ 'utilitytoolcomponentswift', 'walletcomponent']
+for x in list1:
+	print(work_dir)
+	file_path = os.path.join(work_dir, x) # 拼接完整的路径
 
 	os.chdir(file_path)
-	result = os.system("git status")
+	os.system("git status")
 
 	os.system("git checkout" + " " + currentBranch)
 
-	message = input("'库' + item + '输入修改的内容(默认:fixBug) :'")
+	message = input('库:' + x + '输入修改的内容(默认:fixBug) :')
 	if len(message) == 0:
 		message = "fixBug"
 	os.system('git add .')
 	os.system("git commit -m " + message)
-	os.system('git push')
+	os.system('git push origin ' + currentBranch)
+
 
 
